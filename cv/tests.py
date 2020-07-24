3a0ec6c52c1a535e80a9771e66330ac6997a7e4d
Declan <declan.j.fisher@gmail.com>
2020-07-24 14:00:52 +0100

    added cv css

--- cv/tests.py
+++ cv/tests.py
@@ -4,5 +4,5 @@ from cv.views import cv_list
 # Create your tests here.
 class CVPageTest(TestCase):
     def test_cv_url_resolves_to_cv_list_view(self):
-        found = resolve('/cv')
+        found = resolve('cv/')
         self.assertEquals(found.func, cv_list)
--- cv/views.py
+++ cv/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import CVPost
 
 # Create your views here.
 def cv_list(request):
-    return render(request, 'cv/cv_list.html', {})
+    cvPost = CVPost.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'cv/cv_list.html', {'cvPost': cvPost})
